2d1161a5ee644bd334d3caf7e9011993a1376ca2
Ted Neben <tedneben@gmail.com>
2020-05-07 22:00:26 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_concat(array, new_array) 
+  array.concat(new_array)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
